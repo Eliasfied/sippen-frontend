4eabb101e9df483b033930fe2c181b74c20cb95f
Eliasfied <eljop.imba@gmail.com>
2023-05-25 17:40:27 +0200

    -design changes startseite

--- src/components/Startseite.vue
+++ src/components/Startseite.vue
@@ -1,12 +1,7 @@
 <template>
     <ion-page>
-      <ion-header>
-        <ion-toolbar>
-          <ion-title>Neues Spiel</ion-title>
-        </ion-toolbar>
-      </ion-header>
-  
       <ion-content>
+        <div class="page">
         <ion-list>
           <ion-item v-for="(spieler, index) in spielerStore.players" :key="index">
             {{ spieler.name }}
@@ -15,7 +10,7 @@
             </ion-button>
           </ion-item>
         </ion-list>
-  
+        <div class="footer">
         <ion-input v-model="newPlayer" placeholder="Spieler hinzufügen" />
   
         <ion-button @click="addPlayer" :disabled="!newPlayer || spielerStore.players.length >= 10">
@@ -25,6 +20,8 @@
         <ion-button @click="startGame" :disabled="spielerStore.players.length === 0">
           Spiel starten
         </ion-button>
+        </div>
+      </div>
       </ion-content>
     </ion-page>
   </template>
@@ -53,3 +50,45 @@
     router.push("/Spielfenster");
   }
   </script>
+
+<style scoped>
+.page {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #6DB33F; /* grüner Hintergrund */
+  height: 100%;
+}
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+
+ion-list {
+  max-width: 500px; /* oder einen anderen gewünschten Wert */
+  width: 100%;
+  background-color: #6DB33F;
+}
+
+ion-item {
+  color: white; /* weiße Textfarbe */
+}
+
+ion-button {
+  --background: white; /* weiße Hintergrundfarbe */
+  --color: #6DB33F; /* grüne Textfarbe */
+  margin-bottom: 10px;
+}
+
+ion-input {
+  --background: white; 
+  --color: #6DB33F; /* grüner Text */
+  --placeholder-color: #6DB33F; /* grüner Platzhalter */
+  margin-bottom: 10px;
+}
+</style>
