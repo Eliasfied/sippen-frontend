fe6861064ee5fb10ce1faab234ab9d610dab6e7b
Eliasfied <eljop.imba@gmail.com>
2023-05-25 17:56:07 +0200

    startseite ui changes cards

--- src/components/Startseite.vue
+++ src/components/Startseite.vue
@@ -1,55 +1,75 @@
 <template>
-    <ion-page>
-      <ion-content>
-        <div class="page">
+  <ion-page>
+    <ion-content>
+      <div class="page">
         <ion-list>
-          <ion-item v-for="(spieler, index) in spielerStore.players" :key="index">
-            {{ spieler.name }}
-            <ion-button fill="clear" slot="end" @click="removePlayer(index)">
-              <ion-icon :icon="trashOutline" />
-            </ion-button>
-          </ion-item>
+          <ion-card
+            v-for="(spieler, index) in spielerStore.players"
+            :key="index"
+          >
+            <ion-item>
+              <ion-label class="spieler-color">{{ spieler.name }}</ion-label>
+              <ion-button fill="clear" slot="end" @click="removePlayer(index)">
+                <ion-icon :icon="trashOutline" />
+              </ion-button>
+            </ion-item>
+          </ion-card>
         </ion-list>
         <div class="footer">
-        <ion-input v-model="newPlayer" placeholder="Spieler hinzufügen" />
-  
-        <ion-button @click="addPlayer" :disabled="!newPlayer || spielerStore.players.length >= 10">
-          Spieler hinzufügen
-        </ion-button>
-  
-        <ion-button @click="startGame" :disabled="spielerStore.players.length === 0">
-          Spiel starten
-        </ion-button>
+          <ion-input v-model="newPlayer" placeholder="Spieler hinzufügen" />
+
+          <ion-button
+            @click="addPlayer"
+            :disabled="!newPlayer || spielerStore.players.length >= 10"
+          >
+            Spieler hinzufügen
+          </ion-button>
+
+          <ion-button
+            @click="startGame"
+            :disabled="spielerStore.players.length === 0"
+          >
+            Spiel starten
+          </ion-button>
         </div>
       </div>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from "vue";
-  import { IonButton, IonIcon, IonInput, IonItem, IonList, IonPage, IonTitle, IonToolbar } from "@ionic/vue";
-  import { trashOutline } from 'ionicons/icons';
-  import { useSpielerStore } from "../store/SpielerStore";
-  import { useRouter } from "vue-router";
-  
-  const spielerStore = useSpielerStore();
-  const newPlayer = ref("");
-  const router = useRouter();
-  
-  function addPlayer() {
-    spielerStore.addPlayer({ name: newPlayer.value });
-    newPlayer.value = "";
-  }
-  
-  function removePlayer(index : number) {
-    spielerStore.removePlayer(index);
-  }
-  
-  function startGame() {
-    router.push("/Spielfenster");
-  }
-  </script>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import {
+  IonButton,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/vue";
+import { trashOutline } from "ionicons/icons";
+import { useSpielerStore } from "../store/SpielerStore";
+import { useRouter } from "vue-router";
+
+const spielerStore = useSpielerStore();
+const newPlayer = ref("");
+const router = useRouter();
+
+function addPlayer() {
+  spielerStore.addPlayer({ name: newPlayer.value });
+  newPlayer.value = "";
+}
+
+function removePlayer(index: number) {
+  spielerStore.removePlayer(index);
+}
+
+function startGame() {
+  router.push("/Spielfenster");
+}
+</script>
 
 <style scoped>
 .page {
@@ -57,7 +77,7 @@
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  background-color: #6DB33F; /* grüner Hintergrund */
+  background-color: #6db33f; /* grüner Hintergrund */
   height: 100%;
 }
 
@@ -72,23 +92,36 @@
 ion-list {
   max-width: 500px; /* oder einen anderen gewünschten Wert */
   width: 100%;
-  background-color: #6DB33F;
+  background-color: #6db33f;
+}
+
+ion-card {
+  --background: white;
+  --color: #6db33f;
 }
 
 ion-item {
   color: white; /* weiße Textfarbe */
 }
 
+.spieler-color {
+  color:#6db33f;
+}
+
+ion-icon {
+  color: red;
+}
+
 ion-button {
   --background: white; /* weiße Hintergrundfarbe */
-  --color: #6DB33F; /* grüne Textfarbe */
+  --color: #6db33f; /* grüne Textfarbe */
   margin-bottom: 10px;
 }
 
 ion-input {
-  --background: white; 
-  --color: #6DB33F; /* grüner Text */
-  --placeholder-color: #6DB33F; /* grüner Platzhalter */
+  --background: white;
+  --color: #6db33f; /* grüner Text */
+  --placeholder-color: #6db33f; /* grüner Platzhalter */
   margin-bottom: 10px;
 }
 </style>
